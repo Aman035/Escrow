ca48ac0a76b8a23b62f45f960277b15f5018203d
aman035 <guptaaman200115@gmail.com>
2024-05-24 20:33:41 +0530

    add: fn implementations

--- Escrow.sol
+++ Escrow.sol
@@ -3,10 +3,16 @@ pragma solidity 0.8.20;
 
 contract Escrow {
     /* Errors */
+    error Escrow__Unauthorized(address expected, address actual);
+    error Escrow__IncorrectPaymentAmount();
+    error Escrow__IncorrectState(EscrowState expected, EscrowState actual);
+    error Escrow__TransactionFailed();
+    error Escrow__IncorrectDisputeWinner();
 
     /* Interface */
 
     /* Events */
+    event Escrow_Transaction_Successful(address buyer, address seller, uint256 amount);
 
     /* Type Declarations */
     enum EscrowState {
@@ -46,18 +52,44 @@ contract Escrow {
         currentState = EscrowState.AWAITING_PAYMENT;
     }
 
-    /**
-     * @dev - Inplementation Pending
-     */
-    function sendPayment() public payable onlyBuyer {}
+    function sendPayment() public payable onlyBuyer {
+        if (currentState != EscrowState.AWAITING_PAYMENT) {
+            revert Escrow__IncorrectState(EscrowState.AWAITING_PAYMENT, currentState);
+        }
+        if (msg.value != price) {
+            revert Escrow__IncorrectPaymentAmount();
+        }
+        currentState = EscrowState.AWAITING_DELIVERY;
+    }
 
-    /**
-     * @dev - Inplementation Pending
-     */
-    function confirmDelivery() public payable onlyBuyer {}
+    function confirmDelivery() public payable onlyBuyer {
+        if (currentState != EscrowState.AWAITING_DELIVERY) {
+            revert Escrow__IncorrectState(EscrowState.AWAITING_DELIVERY, currentState);
+        }
+        currentState = EscrowState.COMPLETE;
 
-    /**
-     * @dev - Inplementation Pending
-     */
-    function resolveDispute(address winner) public onlyAgent {}
+        emit Escrow_Transaction_Successful(buyer, seller, price);
+
+        (bool success,) = seller.call{value: price}("");
+        if (!success) {
+            revert Escrow__TransactionFailed();
+        }
+    }
+
+    function resolveDispute(address winner) public onlyAgent {
+        if (currentState != EscrowState.AWAITING_DELIVERY) {
+            revert Escrow__IncorrectState(EscrowState.AWAITING_DELIVERY, currentState);
+        }
+
+        if (winner != buyer && winner != seller) {
+            revert Escrow__IncorrectDisputeWinner();
+        }
+
+        currentState = EscrowState.COMPLETE;
+
+        (bool success,) = winner.call{value: price}("");
+        if (!success) {
+            revert Escrow__TransactionFailed();
+        }
+    }
 }
